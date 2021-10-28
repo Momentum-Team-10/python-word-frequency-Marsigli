7d305d82ccd8e66f3f9cb11a88eae3a493e94687
Marsigli <marsigli1988@gmail.com>
2021-10-27 20:32:31 -0400

    displays as the readme credit to james moore hahha

--- word_frequency.py
+++ word_frequency.py
@@ -1,32 +1,10 @@
-# - remove punctuation
-# - normalize all words to lowercase
-# - remove "stop words" -- words used so frequently they are ignored
-# - go through the file word by word and keep a count of how often each word is used
 
-# When your program is complete, you should be able to run 
-# ```
-# python3 word_frequency.py praise_song_for_the_day.txt
-# ``` 
-# and get a printed report like this:
-
-# ```
-#      we | 7 *******
-#    each | 5 *****
-#      or | 5 *****
-#    need | 5 *****
-#    love | 5 *****
-#   about | 4 ****
-#  praise | 4 ****
-#    song | 4 ****
-#     day | 3 ***
-#     our | 3 ***
 
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has', 'he',
     'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to', 'were',
     'will', 'with'
 ]
-
 
 
 def print_word_freq(file):
@@ -48,7 +26,7 @@ def print_word_freq(file):
         text_string = text_string.replace("\"", "")
         text_string = text_string.lower()
         words_list = text_string.split()
-        no_stop_words = []
+        no_stop_words = []    
         for word in words_list:
             if word in STOP_WORDS:
                 pass
@@ -57,7 +35,8 @@ def print_word_freq(file):
         clean_list = {}
         for word in no_stop_words:
             clean_list[word] = no_stop_words.count(word)
-        print(clean_list)
+            print(f"{word} | {clean_list.get(word)} {'*' * int(clean_list.get(word))}")
+        
 
 
 if __name__ == "__main__":
